120ee4a6fbced8eb36adacf7e3ef14f29541e133
ThibaudHardy <thibaudhardy1@gmail.com>
2018-01-29 16:47:51 +0100

    Added functions

--- Calculator/src/Calculatormain.java
+++ Calculator/src/Calculatormain.java
@@ -1,8 +1,23 @@
 
-public class Calculatormain {
-
-	public static void main(String[] args) {
-		System.out.println("Test");
+public class Calculatormain{
+	
+	int nbr1 = 0;
+	int nbr2 = 0;
+	public int addition(int nbr1,int  nbr2){
+		System.out.println(nbr1 + nbr2);
+		return 0;
 	}
-
+	public int substraction(int nbr1,int nbr2) {
+		System.out.println(nbr1 - nbr2);
+		return 0;
+	}
+	public int multiplication(int nbr1,int nbr2){
+		System.out.println(nbr1 * nbr2);
+		return 0;
+	}
+	public int division(int nbr1,int nbr2) {
+		System.out.println(nbr1 / nbr2);
+		return 0;
+	}
 }
+	
